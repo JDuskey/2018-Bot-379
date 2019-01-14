2289a862a0a4f5dfce080c5919f0cd9bc4d43a03
Justin Hamilton <20.hamilton.justin@girardcityschools.org>
2019-01-14 17:25:49 -0500

    Elevators Stick

--- robot.py
+++ robot.py
@@ -8,6 +8,7 @@ class MyRobot(magicbot.MagicRobot):
 	def createObjects(self):
 
                 self.leftStick = wpilib.Joystick(2)
+                self.elevatorStick = wpilib.Joystick(1)
                 self.rightStick = wpilib.Joystick(0)
                 self.elevatorMotorOne = wpilib.Victor(2)
                 self.elevatorMotorTwo = wpilib.Victor(3)
@@ -31,6 +32,7 @@ class MyRobot(magicbot.MagicRobot):
             	self.gearshift.set(2)
             elif(self.gearshift.get() == 2):
                 self.gearshift.set(1)
+		self.elevator.set(self.elevatorStick.getY())
                  
 if __name__ == '__main__':
 	wpilib.run(MyRobot)

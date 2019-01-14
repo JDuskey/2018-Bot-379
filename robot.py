5e4110c15f781411b567ddf859584010d50f420a
Justin Hamilton <20.hamilton.justin@girardcityschools.org>
2019-01-14 16:27:10 -0500

    First Commit
    First Commit

--- robot.py
+++ robot.py
@@ -1,11 +1,18 @@
 import magicbot
 import wpilib
-
 class MyRobot(magicbot.MagicRobot):
 
 	def createObjects(self):
-		pass
+                
+                self.leftStick = wpilib.Joystick(2)
+                self.rightStick = wpilib.Joystick(0)
+                self.elevatorMotorOne = wpilib.Victor(2)
+                self.elevatorMotorTwo = wpilib.Victor(3)
 
+
+                self.elevator = wpilib.SpeedControllerGroup(self.elevatorMotorOne, self.elevatorMotorTwo)
+	
+                self.elevatorPot = wpilib.AnalogPotentiometer(0)
 	def teleopInit(self):
 		pass
 

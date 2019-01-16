4804e7e64940109b1785599d5e73a186480b4e6f
jduskey <jarrettduskey@gmail.com>
2019-01-15 22:09:48 -0500

    Cleaned up code base and indentations.

--- robot.py
+++ robot.py
@@ -3,36 +3,39 @@ import wpilib
 from wpilib.drive import DifferentialDrive
 from robotpy_ext.control.button_debouncer import ButtonDebouncer
 
+
 class MyRobot(magicbot.MagicRobot):
 
-	def createObjects(self):
+    def createObjects(self):
 
-                self.leftStick = wpilib.Joystick(2)
-                self.elevatorStick = wpilib.Joystick(1)
-                self.rightStick = wpilib.Joystick(0)
-                self.elevatorMotorOne = wpilib.Victor(2)
-                self.elevatorMotorTwo = wpilib.Victor(3)
-                self.left = wpilib.Victor(0)
-                self.right = wpilib.Victor(1)
-                self.myRobot = DifferentialDrive(self.left, self.right)
-                self.elevator = wpilib.SpeedControllerGroup(self.elevatorMotorOne, self.elevatorMotorTwo)
-                self.elevatorPot = wpilib.AnalogPotentiometer(0)
+        self.leftStick = wpilib.Joystick(2)
+        self.elevatorStick = wpilib.Joystick(1)
+        self.rightStick = wpilib.Joystick(0)
+        self.elevatorMotorOne = wpilib.Victor(2)
+        self.elevatorMotorTwo = wpilib.Victor(3)
+        self.left = wpilib.Victor(0)
+        self.right = wpilib.Victor(1)
+        self.myRobot = DifferentialDrive(self.left, self.right)
+        self.elevator = wpilib.SpeedControllerGroup(self.elevatorMotorOne, self.elevatorMotorTwo)
+        self.elevatorPot = wpilib.AnalogPotentiometer(0)
 
-                self.gearshift = wpilib.DoubleSolenoid(0,0,1)
-                self.trigger = ButtonDebouncer(self.rightStick,1,period=.5)
-                self.gearshift.set(1)
+        self.gearshift = wpilib.DoubleSolenoid(0, 0, 1)
+        self.trigger = ButtonDebouncer(self.rightStick, 1, period=.5)
+        self.gearshift.set(1)
 
-	def teleopInit(self):
-                pass
+    def teleopInit(self):
+        pass
 
-	def teleopPeriodic(self):
+    def teleopPeriodic(self):
+
         self.myRobot.tankDrive(-self.leftStick.getY(), -self.rightStick.getY())
-        if(self.trigger.get()):
-            if(self.gearshift.get() == 1):
+        if (self.trigger.get()):
+            if (self.gearshift.get() == 1):
                 self.gearshift.set(2)
-            elif(self.gearshift.get() == 2):
-                self.gearshift.set(1)
-		self.elevator.set(self.elevatorStick.getY())
-                 
+        elif (self.gearshift.get() == 2):
+            self.gearshift.set(1)
+        self.elevator.set(self.elevatorStick.getY())
+
+
 if __name__ == '__main__':
-	wpilib.run(MyRobot)
+    wpilib.run(MyRobot)
